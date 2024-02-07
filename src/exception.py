792372ef24377f0a05297cc9f79aa4906261e7a6
Deep Korat <deepkorat13@gmail.com>
2024-02-07 18:46:44 +0530

    add data into new notebook folder

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys): 
      # exc_tb contains all information of error - in which line, what error occuring.    
@@ -7,7 +7,6 @@ def error_message_details(error, error_detail:sys):
      file_name = exc_tb.tb_frame.f_code.co_filename
      error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
           file_name, exc_tb.tb_lineno, str(error)
-
      )
      return error_message
  
@@ -20,3 +19,9 @@ class CustomException(Exception):
           return self.error_message
 
      
+if __name__ == "__main__":
+     try:
+          a = 0/1
+     except Exception as e:
+          logging.basicConfig.info("Divide by Zero")
+          raise CustomException(e, sys)
