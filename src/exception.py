c3e49b977de4780c29c19a5c723bf42c2319e399
Deep Korat <93854770+deepkorat@users.noreply.github.com>
2024-02-22 07:50:03 +0000

    solve src error in exception and logger file

--- src/exception.py
+++ src/exception.py
@@ -25,7 +25,7 @@ class CustomException(Exception):
      
 if __name__ == "__main__":
      try:
-          a = 0/1
+          a = 1/0
      except Exception as e:
-          logging.basicConfig.info("Divide by Zero")
+          logging.info("Divide by Zero")
           raise CustomException(e, sys)
